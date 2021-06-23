6e00b7c0b4c331ef4ca3591dc7ec03b5fce69c70
tdenis29 <tdenis29@outlook.com>
2021-06-23 11:27:10 -0700

    morestuff

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //Another OOP library!
+let myLibrary = [];
+
 class Book {
     constructor(title, author, pages, read, _logInfo) {
             this.title = title,
@@ -10,6 +12,31 @@ class Book {
         };
     }
 }
+//add book to myLibrary array
+function addBook(){
+
+}
+//remove book from array
+function removeBook(){
+
+}
+//toggles read status on book prototype inheritiance
+function readBook(){
+
+}
+//loop through myLibrary array to display each book
+function dispalyLibrary(){
+
+}
+//SET books to local sotrage 
+function saveBook(){
+
+}
+// GET books from local storage
+function loadBooks(){
+
+}
+//book instance
 const book1 = new Book("The Hobbit", "J.R.R Tolkein", "295", "Yes");
 
 console.log(book1.logInfo());
