fcc8fcdc4e7bcc155c99d59bcbddc146b04de7f9
tdenis29 <tdenis29@outlook.com>
2021-07-03 15:02:34 -0700

    holyf!itkindaworks!

--- app.js
+++ app.js
@@ -3,47 +3,32 @@ const overlay = document.getElementById('overlay');
 const addNewBook = document.getElementById('addNewBook');
 const send = document.getElementById('send');
 const form = document.getElementById("form");
+const localStorage = window.localStorage;
+
 
 //Another OOP library!
-let myLibrary = [
-    // {
-    // title: "The Hobbit",
-    // author: "JRR Tolkien",
-    // pages: 295,
-    // read: "on",
-    // },
-    // {
-    // title: "Dune",
-    // author: "Herbert",
-    // pages: 500,
-    // read: "off",
-    // },
-    // {
-    // title: "Great Expectations",
-    // author: "Charles Dickens",
-    // pages: 650,
-    // read: "on",
-    // },
-    // {
-    // title: "50 Shades Of Grey",
-    // author: "Who Cares",
-    // pages: 10,
-    // read: "off",
-    // }
-];
+let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read,) {
+    constructor( title, author, pages, read) {
             this.title = title,
             this.author = author,
             this.pages = pages,
-            this.read = read,
-            this.readBookStatus = function () {
-                console.log(this); 
+            this.read = read
+    }
+            readBookStatus = function (){
+                console.log(this)
+                if(this.read === true){
+                    let myCheck = document.getElementById('checkMe');
+                    myCheck.checked = true;
+                } else{
+                    let myCheck = document.getElementById('checkMe');
+                    myCheck.checked = false;
+                }
         };
     }
-}
-window.addEventListener("onload", dispalyLibrary());
+
+// window.addEventListener("onload"); //loadbooks);
 //addNewBook to open modal window
 addNewBook.addEventListener('click', (e) => {
     overlay.style.display = 'block';
@@ -55,24 +40,24 @@ form.addEventListener('submit', function addBookToLibrary(e){
     let bookTitle = document.getElementById('bookTitle').value;
     let bookAuthor = document.getElementById('bookAuthor').value;
     let bookPages = document.getElementById('bookPages').value;
+    let bookRead = document.getElementById("readStatus").checked;
     if(bookTitle !== "" &&
         bookAuthor !== ""  && 
         bookPages !== 0 || bookPages !== ""){
-            let book = new Book(bookTitle, bookAuthor, bookPages);
-            myLibrary.push(book)
+            let book = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+            myLibrary.push(book);
             overlay.style.display = "none";
         }
         //display new book after myLibrary.push()
-        dispalyLibrary();
+        dispalyLibrary()
         //reset form after preventdefault()
         form.reset();
         //calls get cards for lexical reference to remove btns
         let usetheseBtn = getCards();
         //apply event handler to newly created buttons
         usetheseBtn.forEach(applyEvent);
+        
     });
-
-
 //this function gets current button elements and returns as a variable to pass to applyEvent()
 function getCards(){
 let removeBtns = document.querySelectorAll('.remove');
@@ -81,14 +66,13 @@ return removeBtns;
 }
 function applyEvent(){
     let usetheseBtn = getCards();
-    //mylibSplice is a named function for this reference on clicked button to remove with parentNode
+    //mylibSplice is a named function for !this! reference on clicked button to remove with parentNode
     usetheseBtn.forEach(btn => {btn.addEventListener('click', function mylibSplice() {
         pos = btn.dataset.index;
         parseInt(pos);
-        myLibrary.splice(pos, 1);
+        myLibrary = myLibrary.slice(pos, 1) 
         this.parentNode.parentNode.remove();
 })})};
-
 //loop through myLibrary array to display each book
 function dispalyLibrary(){
     const bookContainer = document.getElementById("book-container");
@@ -99,22 +83,34 @@ function dispalyLibrary(){
         let pages = book.pages;
         let read = book.read;
         bookHTML += `
-        <div class="book-card" data-index="${index}">
-        <h3>${title}</h3>
+        <div class="book-card" id="bookCard" data-index="${index}">
+        <h3 id="bookTitle">${title}</h3>
             <p>${author}</p>
                 <p>${pages}</p>
                 <div class="check-con">
                     <label for="read">Read?</label>
-                    <input id="read" type="checkbox">
-                </div>
-                <div class="book-button-con">
-                    <button onclick="removeBook()">Edit</button>
-                    <button class="remove">Remove</button>
-                </div>  
-            </div>   
-                `               
+                    `;if(book.read === true){
+                       bookHTML += `<input id="checkMe" type="checkbox" checked>`  
+                    }else{
+                       bookHTML += `<input id="checkMe" type="checkbox">`
+                    }
+                bookHTML += `
+                        </div>
+                        <div class="book-button-con">
+                            <button class="remove">Remove</button>
+                        </div>  
+                    </div>   
+             `;   
+             
     });
     bookContainer.innerHTML = bookHTML;
+    // myLibrary.forEach(book => {
+    //     if(document.getElementById('bookTitle').textContent === book.title){
+    //         return
+    //     } else{
+    //     book.readBookStatus();
+    //     }
+    // })
 }
 //SET books to local sotrage 
 function saveBook(){
@@ -128,5 +124,6 @@ function loadBooks(){
 modalClose.addEventListener('click', () => {
     overlay.style.display = "none"
     });
+
 
 
